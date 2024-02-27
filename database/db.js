18e94847a96ec58fbea90560dff029808f9d0b81
Isaac Flores <isaacfloresva@gmail.com>
2024-02-27 14:20:09 -0700

    Delete database directory

--- database/db.js
+++
@@ -1,9 +0,0 @@
-import { DB_HOST, DB_DATABASE, DB_USER, DB_PASSWORD } from "../../d_config.js";
-import { Sequelize } from "sequelize";
-
-const db = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
-  dialect: "mysql",
-});
-
-export default db;
