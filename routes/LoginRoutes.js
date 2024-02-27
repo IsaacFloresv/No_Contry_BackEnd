2651d2936780b7b5de1fb8520f66a5a0ccfedd44
Isaac Flores <isaacfloresva@gmail.com>
2024-02-27 14:20:33 -0700

    Delete routes directory

--- routes/LoginRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { login } from "../controllers/LoginController.js";
-import ValidationsLogin from '../middlewares/validator.js'
-
-const LoginRouter = Router()
-
-LoginRouter.post('/', login);
-
-export default LoginRouter
