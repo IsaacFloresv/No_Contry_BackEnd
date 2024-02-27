472b7998db39d81e6668bc09f50991b93dc59719
Isaac Flores <isaacfloresva@gmail.com>
2024-02-27 10:34:13 -0700

    Update NotesModel.js

--- models/NotesModel.js
+++ models/NotesModel.js
@@ -1,18 +1,17 @@
-//importa la conexion a la db
-import db from "../database/db.js";
-//Importar Sequilize
+import db from "../db.js";
 import { DataTypes } from "sequelize";
 
-
-const notesModel = db.define('Notes', {
-    id: { type: DataTypes.INTEGER, primaryKey: true },
+export const NoteModel = db.define(
+  "Notes",
+  {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     note: { type: DataTypes.STRING },
-    is_public: { type: DataTypes.TINYINT(0,1), defaultValue:'0'},
-    teacher_id: { type: DataTypes.INTEGER, allowNull: false},
-    student_id: { type: DataTypes.INTEGER, allowNull: false},
-    subject_id: { type: DataTypes.INTEGER, allowNull: false},
-},{
-    timestamps: false
-})
-
-export default notesModel
+    is_public: { type: DataTypes.TINYINT(0, 1), defaultValue: "0" },
+    teacher_id: { type: DataTypes.INTEGER, allowNull: false },
+    student_id: { type: DataTypes.INTEGER, allowNull: false },
+    subject_id: { type: DataTypes.INTEGER, allowNull: false },
+  },
+  {
+    timestamps: true,
+  }
+);
